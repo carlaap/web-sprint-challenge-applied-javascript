020ce59474651951855b77ff8be75ee7ce668735
Byron Francisco Portillo <byronportillo@byrons-mbp.attlocal.net>
2022-04-14 22:16:38 -0700

    Finsihed Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,28 @@
 const Header = (title, date, temp) => {
+  // Create tags
+  const div = document.createElement('div');
+  const spanDate = document.createElement('span');
+  const h1 = document.createElement('h1');
+  const spanTemp = document.createElement('span');
+
+  // Add classes
+  div.classList.add('header');
+  spanDate.classList.add('date');
+  spanTemp.classList.add('temp');
+
+  // Add texts to the tags
+  h1.textContent = title;
+  spanDate.textContent = date;
+  spanTemp.textContent = temp;
+
+  // Append to their parents
+  div.appendChild(spanDate);
+  div.appendChild(h1);
+  div.appendChild(spanTemp);
+
+  // Return header
+  return div;
+}
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -11,15 +35,18 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
-}
+
+//}
 
 const headerAppender = (selector) => {
+    const header = Header('Lambda Times', '04/14/2022', '60');
+    document.querySelector(selector).appendChild(header);
+}
+export { Header, headerAppender }
+
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-}
-
-export { Header, headerAppender }
